6ba00d35ad8a1a3a91e16af88505d7d6813a0a8e
kellyjovan <jovanjkelly@yahoo.com>
2014-11-26 17:16:52 -0500

    Created Hospital class

---
+++ hospital.rb
@@ -0,0 +1,32 @@
+class Hospital
+  def initialize(name, location)
+    @name = name
+    @rating = 5 
+    @location = location
+    menu
+  end
+  def menu
+    puts "1. Add a patient"
+    puts "2. Add a doctor"
+    puts "3. Add a nurse"
+    puts "4. Treat a patient"
+    puts "5. Fire someone"
+    puts "6. Time passed"
+    choice = gets.chomp.to_i
+
+    if choice == 1
+      puts "You are adding a patient"
+    elsif choice == 2
+      puts "You are adding a doctor"
+    elsif choice == 3
+      puts "You are adding a nurse"
+    elsif choice == 4
+      puts "You are treating a patient"
+    elsif choice == 5
+      puts "You are firing someone"
+    elsif choice == 6
+      puts "Time passed"
+    end
+      
+  end
+end
